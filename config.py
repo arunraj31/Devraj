6f0529a13a4bdb8703c74f47f9a81872d8341922
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-11 12:35:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from os import getenv
+
+
+API_ID = int(os.environ.get("API_ID"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH")
+# ----------------D--------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+
+HEROKU_API = os.environ.get("HEROKU_API")
