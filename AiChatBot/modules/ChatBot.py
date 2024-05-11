f6093729af003d1908e97b29f3fa8eff1579c00a
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-11 13:32:24 +0530

    Update ChatBot.py

--- AiChatBot/modules/ChatBot.py
+++ AiChatBot/modules/ChatBot.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from motor.motor_asyncio import AsyncIOMotorClient
 import requests
 from config import *
-from AiChatBot import murali
+from AiChatBot import murali as app
 
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 db = mongo_client.chatbotdbb
@@ -56,5 +56,27 @@ async def enable_disable_chatbot(_, query: types.CallbackQuery):
         # if not adminn 
         await query.answer("You are not an admin in this group.")
 
-
+@app.on_message(filters.group & filters.text & filters.reply)
+async def handle_message(client: Client, message: Message):
+    try:
+        chat_id = message.chat.id
+        
+        # Check 
+        if (message.reply_to_message and message.reply_to_message.from_user.is_self) or not message.reply_to_message:
+            
+            chatbot_info = await chatbotdatabase.find_one({"chat_id": chat_id})
+            if chatbot_info:
+                user_id = message.from_user.id
+                user_message = message.text
+                api_url = f"http://api.brainshop.ai/get?bid=180331&key=1EGyiLpUu4Vv6mwy&uid={user_id}&msg={user_message}"
+                response = requests.get(api_url).json()["cnt"]
+                await message.reply_text(response)
+            else:
+                pass  # If chatbot is not enabled, do nothing
+        else:
+            pass  # If the message doesn't meet the conditions, do nothing.
+    except Exception as e:
+        # Optionally, log the exception if needed
+        print(f"An error occurred: {str(e)}")
+        
 
