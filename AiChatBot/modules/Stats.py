cf89373a865f13d38e3213e34117d5c7e0c0175c
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-12 19:32:28 +0530

    Create Stats.py

---
+++ AiChatBot/modules/Stats.py
@@ -0,0 +1,18 @@
+from AiChatBot import Chiku
+from config import OWNER_ID 
+from AiChatBot.db import get_served_chats, get_served_chats
+from pyrogram import client, filters
+from pyrogram.types import Message
+
+
+@Chiku.on_message(filters.command("stats") & filters.user(OWNER_ID))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {Chiku.mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}
+"""
+    )
