9b92acc14c3d238e33b15914d350b10a58dea55e
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-12 14:12:06 +0530

    Update ChatBot.py

--- AiChatBot/modules/ChatBot.py
+++ AiChatBot/modules/ChatBot.py
@@ -96,27 +96,3 @@ async def handle_message(client, message: types.Message):
         await message.reply_text(result)
 
 
-TEXT = f"""
-@app.on_message(filters.text & ~filters.bot & ~filters.private)
-async def handlepvt_message(client, message):
-    try:
-        if (
-            message.text.startswith("!")
-            or message.text.startswith("/")
-            or message.text.startswith("?")
-            or message.text.startswith("@")
-            or message.text.startswith("#")
-            or message.text.startswith("P")
-        ):
-            return
-        else:
-            user_id = message.from_user.id
-            user_message = message.text
-            api_url = f"http://api.brainshop.ai/get?bid=180331&key=1EGyiLpUu4Vv6mwy&uid={user_id}&msg={user_message}"
-            response = requests.get(api_url).json()["cnt"]
-            await client.send_chat_action(message.chat.id, ChatAction.TYPING)
-            await message.reply_text(response)
-    except Exception as e:
-        # Optionally, log the exception if needed
-        print(f"An error occurred: {str(e)}")
-"""
