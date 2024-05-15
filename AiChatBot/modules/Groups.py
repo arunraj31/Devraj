d08e6813d457670b8787399b57125e78180172ab
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-15 16:51:50 +0530

    Update Groups.py

--- AiChatBot/modules/Groups.py
+++ AiChatBot/modules/Groups.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from AiChatBot import Chiku as app
-from AiChatBot import LOGGER_ID as LOG_GRIUP_ID
+from AiChatBot import LOGGER_ID as LOG_GROUP_ID
 from AiChatBot.Db import get_served_chats, remove_served_chat, add_served_chat
 
 import requests
