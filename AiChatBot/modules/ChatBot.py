aaa48bad090d2dff68cea8f72353bb01b7e3f03a
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-15 22:05:12 +0530

    Update ChatBot.py

--- AiChatBot/modules/ChatBot.py
+++ AiChatBot/modules/ChatBot.py
@@ -87,6 +87,8 @@ async def handle_message(client, message):
     user_id = message.from_user.id
     if "Developer" in message.text:
         return await message.reply_text("My Developer Is My Cute Owner Murali (@ZeroXCoderz)")
+    if "I Hate You" in message.text:
+        return await message.reply_text("Please tell me what I did to make you angry? \nYour Feedback Is Important To Me \nPlease Report To My Devoloper")
     if message.chat.type == ChatType.PRIVATE:
         for emoji in Emojis:
             if emoji in message.text:
