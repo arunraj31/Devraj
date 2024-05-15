f8b85768011449eee6d3536b5db5a21166f827f3
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-15 12:46:36 +0530

    Create Gcast.py

---
+++ AiChatBot/modules/Gcast.py
@@ -0,0 +1,76 @@
+from AiChatBot import Chiku
+import asyncio
+from pyrogram import client, filters
+from config import OWNER_ID
+from pyrogram.errors import FloodWait
+
+@Chiku.on_message(filters.command(["broadcast", "Gcast"]))
+async def braodcast_message(client, message, _):
+    if message.from_user.id not in OWNER_ID:
+        return await message.reply_text(
+            "» 😁 ʜᴇʜᴇʜᴇ ᴏɴʟʏ ᴍʏ ᴏᴡɴᴇʀ ᴄᴀɴ ʙʀᴏᴀᴅᴄᴀsᴛ"
+        )
+    global IS_BROADCASTING
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text(f"**Usage**/n\broadcast Message or reply " )
+        query = message.text.split(None, 1)[1]
+        if "-pin" in query:
+            query = query.replace("-pin", "")
+        if "-nobot" in query:
+            query = query.replace("-nobot", "")
+        if "-pinloud" in query:
+            query = query.replace("-pinloud", "")
+        if "-assistant" in query:
+            query = query.replace("-assistant", "")
+        if "-user" in query:
+            query = query.replace("-user", "")
+        if query == "":
+            return await message.reply_text("give text to broadcast something")
+
+    IS_BROADCASTING = True
+
+    # Bot broadcast inside chats
+    if "-nobot" not in message.text:
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            if i == -1002113460681:
+                continue
+            try:
+                m = (
+                    await Chiku.forward_messages(i, y, x)
+                    if message.reply_to_message
+                    else await Chiku.send_message(i, text=query)
+                )
+                if "-pin" in message.text:
+                    try:
+                        await m.pin(disable_notification=True)
+                        pin += 1
+                    except Exception:
+                        continue
+                elif "-pinloud" in message.text:
+                    try:
+                        await m.pin(disable_notification=False)
+                        pin += 1
+                    except Exception:
+                        continue
+                sent += 1
+            except FloodWait as e:
+                flood_time = int(e.x)
+                if flood_time > 200:
+                    continue
+                await asyncio.sleep(flood_time)
+            except Exception:
+                continue
+        try:
+            await message.reply_text("broadcasted message in {} chats with {} pins from bot ")
+        except:
+            pass
