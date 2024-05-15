a2bf8f905d4c47c0f302dea3b1ba443c743bd002
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-15 13:55:06 +0530

    Update Stats.py

--- AiChatBot/modules/Stats.py
+++ AiChatBot/modules/Stats.py
@@ -4,15 +4,18 @@ from AiChatBot.Db import get_served_chats, get_served_users
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
 @Chiku.on_message(filters.command("stats") & filters.user(OWNER_ID))
 async def stats(cli: Client, message: Message):
-    users = len(await get_served_users())
-    chats = len(await get_served_chats())
-    await message.reply_text(
-        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {Chiku.mention} :
+    try:
+        users = len(await get_served_users())
+        chats = len(await get_served_chats())
+        await message.reply_text(
+            f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {Chiku.mention} :
 
 ➻ **ᴄʜᴀᴛs :** {chats}
 ➻ **ᴜsᴇʀs :** {users}
 """
-    )
+        )
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        await message.reply_text("An error occurred while gathering stats.")
