4cdb70ba572525bbc655240fc544710d94f75cb1
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-16 17:22:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ from os import getenv
 
 
 API_ID = int(os.environ.get("API_ID"))
-# ------------------------------------------------
+
+
 API_HASH = os.environ.get("API_HASH")
-# ----------------D--------------------------------
+
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 OWNER_ID = list(
@@ -15,3 +17,5 @@ OWNER_ID = list(
 HEROKU_API = os.environ.get("HEROKU_API")
 
 MONGO_URL = os.environ.get("MONGO_URL")
+
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002113460681"))
