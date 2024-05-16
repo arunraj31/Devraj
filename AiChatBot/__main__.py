b3e360618509e804c8e7a1ffc86cfe43ca542649
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-16 17:27:57 +0530

    Update __main__.py

--- AiChatBot/__main__.py
+++ AiChatBot/__main__.py
@@ -9,8 +9,8 @@ loop = asyncio.get_event_loop()
 async def pikachuu():
     for all_module in ALL_MODULES:
         importlib.import_module("AiChatBot.modules." + all_module)
-    print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
-    await Chiku.send_message(LOGGER_ID, "chat bot start 🌋")
+    print("𝐂𝐇𝐈𝐊𝐔 𝐁𝐎𝐓 𝐇𝐀𝐒 𝐁𝐄𝐄𝐍 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ✨")
+    await Chiku.send_message(LOGGER_ID, "ᴄʜᴀᴛʙᴏᴛ ɪs sᴛᴀʀᴛᴇᴅ 🌋 \n\nMʏ Cᴜᴛᴇ Dᴇᴠᴇʟᴏᴘᴇʀ @zeroxcoderz")
     await idle()
     
 if __name__ == "__main__":
